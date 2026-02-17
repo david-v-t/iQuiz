e561554b4fd12c357d15b93764fec59f703d27cd
David <davidvt@uw.edu>
2026-02-17 15:39:30 -0800

    initial app

---
+++ iQuiz/iQuiz/iQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizApp.swift
+//  iQuiz
+//
+//  Created by iguest on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct iQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
